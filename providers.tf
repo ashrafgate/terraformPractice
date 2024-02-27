274c50a4f8fbc53f66d6bc8d32a3bf7a4e569e18
ashrafgate <66882690+ashrafgate@users.noreply.github.com>
2024-02-27 11:11:09 +0530

    initial commit

---
+++ providers.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.37.0"
+    }
+  }
+}
+
+# create resources in North Virginia
+provider "aws" {
+  # Configuration options
+  region = "us-east-1"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "b80-adv-terraform-bucket"
+    key    = "tf-current-state-file"
+    region = "us-east-1"
+  }
+}
